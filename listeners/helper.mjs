a0aa3d90765e061e4d76a04a2d923241fc9acfbe
Hongzhi Wen <wenguanjung@aliyun.com>
2023-03-12 14:18:31 -0400

    Update helper.mjs

--- listeners/helper.mjs
+++ listeners/helper.mjs
@@ -1,12 +1,9 @@
 import { ChatGPTAPI } from 'chatgpt'
 const api = new ChatGPTAPI({
-    apiKey: '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'
+    apiKey: ''
   })
 
 export async function example(text, id){ 
-//  const api = new ChatGPTAPI({
-//    apiKey: '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'
-//  })
   
   if (id==='none'){
 	const res = await api.sendMessage(text);
